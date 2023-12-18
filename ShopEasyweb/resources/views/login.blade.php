d69185ef5cd5bfa3ddd3ca8f9593aeee2234424e
Reyvanatalla <atallareyvan4@gmail.com>
2023-12-18 21:53:06 +0700

    ShopEasy

---
+++ ShopEasyweb/resources/views/login.blade.php
@@ -0,0 +1,332 @@
+<div class="div">
+    <div class="div-2">
+      <div class="div-3">
+        <div class="div-4">
+          <div class="column">
+            <img
+              loading="lazy"
+              srcset="..."
+              class="img"
+            />
+          </div>
+          <div class="column-2">
+            <div class="div-5">
+              <div class="div-6">Log in</div>
+              <div class="div-7">Username/Email</div>
+              <div class="div-8">
+                <div class="div-9">Password</div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/eadcb6d9c7c9a1e63982f34cb64d553867e8c60a413c350ea16bc4b1d847df97?"
+                  class="img-2"
+                />
+              </div>
+              <a href="Dashboard.htm"><div class="div-10">LOG IN</div></a>
+            
+              <div class="div-11">Lupa Password?</div>
+              <div class="div-12">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/7bc05e0459c913378b6979fd1254d89d9bbed3945bffafea35f969274bd8dfdc?"
+                  class="img-3"
+                />
+                <div class="div-13">ATAU</div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/2641e138253d245029348dcf8b2db2f778ae826ae87651ce4132cef770bfe32b?"
+                  class="img-4"
+                />
+              </div>
+              <div class="div-14">
+                <img
+                  loading="lazy"
+                  srcset="..."
+                  class="img-5"
+                />
+                <div class="div-15">Google</div>
+              </div>
+              <div class="div-16">
+                <div class="div-17">Baru di ShopEasy?</div>
+                <div class="div-18">Daftar</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <style>
+    .div {
+      background-color: #fff;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .div-2 {
+      background-color: #c2a7a7;
+      display: flex;
+      width: 100%;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 50px 60px;
+    }
+    @media (max-width: 991px) {
+      .div-2 {
+        max-width: 100%;
+        padding: 0 20px;
+      }
+    }
+    .div-3 {
+      width: 100%;
+      max-width: 1020px;
+      margin: 105px 0 74px;
+    }
+    @media (max-width: 991px) {
+      .div-3 {
+        max-width: 100%;
+        margin: 40px 0;
+      }
+    }
+    .div-4 {
+      gap: 20px;
+      display: flex;
+    }
+    @media (max-width: 991px) {
+      .div-4 {
+        flex-direction: column;
+        align-items: stretch;
+        gap: 0px;
+      }
+    }
+    .column {
+      display: flex;
+      flex-direction: column;
+      line-height: normal;
+      width: 49%;
+      margin-left: 0px;
+    }
+    @media (max-width: 991px) {
+      .column {
+        width: 100%;
+      }
+    }
+    .img {
+      aspect-ratio: 1.45;
+      object-fit: contain;
+      object-position: center;
+      width: 100%;
+      overflow: hidden;
+      margin: auto 0;
+    }
+    @media (max-width: 991px) {
+      .img {
+        max-width: 100%;
+        margin-top: 40px;
+      }
+    }
+    .column-2 {
+      display: flex;
+      flex-direction: column;
+      line-height: normal;
+      width: 51%;
+      margin-left: 20px;
+    }
+    @media (max-width: 991px) {
+      .column-2 {
+        width: 100%;
+      }
+    }
+    .div-5 {
+      border-radius: 14px;
+      background-color: #fffdfd;
+      display: flex;
+      flex-grow: 1;
+      flex-direction: column;
+      width: 100%;
+      padding: 41px 62px 41px 38px;
+    }
+    @media (max-width: 991px) {
+      .div-5 {
+        max-width: 100%;
+        margin-top: 40px;
+        padding: 0 20px;
+      }
+    }
+    .div-6 {
+      color: #000;
+      align-self: stretch;
+      font: 400 40px Inter, sans-serif;
+    }
+    .div-7 {
+      color: rgba(0, 0, 0, 0.2);
+      border: 1px solid rgba(64, 64, 64, 0.8);
+      background-color: #fffefe;
+      align-self: stretch;
+      margin-top: 29px;
+      justify-content: center;
+      align-items: start;
+      padding: 20px 60px 20px 18px;
+      font: 400 15px Inter, sans-serif;
+    }
+    @media (max-width: 991px) {
+      .div-7 {
+        padding-right: 20px;
+      }
+    }
+    .div-8 {
+      border: 1px solid rgba(64, 64, 64, 0.8);
+      background-color: #fffefe;
+      align-self: stretch;
+      display: flex;
+      margin-top: 27px;
+      align-items: start;
+      justify-content: space-between;
+      gap: 20px;
+      padding: 17px 10px 11px 18px;
+    }
+    .div-9 {
+      color: rgba(0, 0, 0, 0.2);
+      font: 400 15px Inter, sans-serif;
+    }
+    .img-2 {
+      aspect-ratio: 1;
+      object-fit: contain;
+      object-position: center;
+      width: 24px;
+      overflow: hidden;
+      align-self: stretch;
+      max-width: 100%;
+    }
+    /* .div-10 {
+      color: #fff;
+      white-space: nowrap;
+      border: 1px solid rgba(64, 64, 64, 0.8);
+      background-color: #c4a7a7;
+      align-self: stretch;
+      margin-top: 53px;
+      justify-content: center;
+      align-items: center;
+      padding: 19px 60px;
+      font: 400 20px Inter, sans-serif;
+    } */
+    @media (max-width: 991px) {
+      .div-10 {
+        white-space: initial;
+        margin-top: 40px;
+        padding: 0 20px;
+      }
+    }
+    .div-11 {
+      color: #4e6bff;
+      align-self: stretch;
+      margin-top: 11px;
+      font: 400 13px Inter, sans-serif;
+    }
+    .div-12 {
+      align-self: stretch;
+      display: flex;
+      margin-top: 13px;
+      justify-content: space-between;
+      gap: 15px;
+    }
+    @media (max-width: 991px) {
+      .div-12 {
+        justify-content: center;
+      }
+    }
+    .img-3 {
+      aspect-ratio: 155;
+      object-fit: contain;
+      object-position: center;
+      width: 155px;
+      fill: rgba(0, 0, 0, 0.4);
+      stroke-width: 1px;
+      stroke: rgba(0, 0, 0, 0.4);
+      overflow: hidden;
+      align-self: center;
+      max-width: 100%;
+      margin: auto 0;
+    }
+    .div-13 {
+      color: rgba(0, 0, 0, 0.4);
+      font: 400 11px Inter, sans-serif;
+    }
+    .img-4 {
+      aspect-ratio: 155;
+      object-fit: contain;
+      object-position: center;
+      width: 155px;
+      fill: rgba(0, 0, 0, 0.4);
+      stroke-width: 1px;
+      stroke: rgba(0, 0, 0, 0.4);
+      overflow: hidden;
+      align-self: center;
+      max-width: 100%;
+      margin: auto 0;
+    }
+    .div-14 {
+      border: 1px solid rgba(0, 0, 0, 0.4);
+      background-color: #fdfbfb;
+      align-self: center;
+      display: flex;
+      margin-top: 11px;
+      gap: 8px;
+      padding: 7px 60px 7px 35px;
+    }
+    @media (max-width: 991px) {
+      .div-14 {
+        padding: 0 20px;
+      }
+    }
+    .img-5 {
+      aspect-ratio: 1;
+      object-fit: contain;
+      object-position: center;
+      width: 28px;
+      overflow: hidden;
+      max-width: 100%;
+    }
+    .div-15 {
+      color: #000;
+      align-self: center;
+      flex-grow: 1;
+      white-space: nowrap;
+      margin: auto 0;
+      font: 400 16px Inter, sans-serif;
+    }
+    @media (max-width: 991px) {
+      .div-15 {
+        white-space: initial;
+      }
+    }
+    .div-16 {
+      align-self: center;
+      display: flex;
+      margin-top: 25px;
+      gap: 8px;
+    }
+    .div-17 {
+      color: rgba(0, 0, 0, 0.5);
+      flex-grow: 1;
+      white-space: nowrap;
+      font: 400 13px Inter, sans-serif;
+    }
+    @media (max-width: 991px) {
+      .div-17 {
+        white-space: initial;
+      }
+    }
+    .div-18 {
+      color: #c4a7a7;
+      align-self: start;
+      white-space: nowrap;
+      font: 400 13px Inter, sans-serif;
+    }
+    @media (max-width: 991px) {
+      .div-18 {
+        white-space: initial;
+      }
+    }
+  </style>
+  
